ee41e10def9a56c274d017d87ceca9674e1ab296
PSheon <pauljiang61020@gmail.com>
2021-05-25 18:26:56 +0800

    feat: replace main script into src folder.

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import setupConfig from "./tasks/setup-config";
+import setupDependencies from "./tasks/setup-dependencies";
+import preparingVideo from "./tasks/preparing-video";
+import splitFrames from "./tasks/split-frames";
+import inferenceFrame from "./tasks/inference-frame";
+
+const pipeline = async (): Promise<void> => {
+  /* Environment Setup */
+  setupConfig();
+  await setupDependencies();
+
+  /* Preparing - Preparing Video */
+  await preparingVideo();
+
+  /* Stage 1 - Split Frames */
+  await splitFrames();
+
+  /* Stage 2 - Inference Frame */
+  await inferenceFrame();
+};
+pipeline();
