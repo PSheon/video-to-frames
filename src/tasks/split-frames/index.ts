607e988dd68aba424320b50cee2d5032be09531a
PSheon <pauljiang61020@gmail.com>
2021-06-07 03:15:22 +0800

    feat: added split image to  stage pipeline.

--- src/tasks/split-frames/index.ts
+++ src/tasks/split-frames/index.ts
@@ -1,44 +1,20 @@
-import PROCESS_ENV from "config";
-
 import ora from "ora";
-import path from "path";
-import chalk from "chalk";
-import ffmpeg from "fluent-ffmpeg";
+
+import { splitImageToFrame, splitVideoToFrames } from "./helpers";
 
 export default function (): Promise<void> {
-  return new Promise((resolve) => {
-    const spinner = ora("分割影片中...").start();
-    const baseDirName = global["baseDirName"];
+  return new Promise(async (resolve) => {
+    const spinner = ora("分割文件中...").start();
+    const fileMimeType = global["fileMimeType"];
 
-    ffmpeg(
-      path.resolve(
-        baseDirName,
-        PROCESS_ENV.get("INPUT_FILEPATH"),
-        PROCESS_ENV.get("INPUT_FILENAME"),
-      ),
-    )
-      .on("codecData", (data) => {
-        spinner.text = `Input is ${data.audio} audio with ${data.video} video"`;
-      })
-      .on("error", (err) => {
-        spinner.fail(`${chalk.red("[階段二]")} 影片分割失敗: ${err.message}`);
-        process.exit(1);
-      })
-      .on("end", () => {
-        spinner.succeed(`${chalk.green("[階段二]")} 影片分割完成！`);
-        resolve();
-      })
-      .size("640x?")
-      .aspect("1:1")
-      .autopad()
-      .fps(PROCESS_ENV.get("INPUT_VIDEO_FRAME_SAMPLING"))
-      .save(
-        path.resolve(
-          baseDirName,
-          "output",
-          "stage-split",
-          `${PROCESS_ENV.get("SPLIT_FRAME_IMAGE_PREFIX")}_%05d.jpg`,
-        ),
-      );
+    if (fileMimeType.includes("image")) {
+      await splitImageToFrame({ spinner });
+    }
+
+    if (fileMimeType.includes("video")) {
+      await splitVideoToFrames({ spinner });
+    }
+
+    resolve();
   });
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from "./setup-config";
 export * from "./setup-dependencies";
 export * from "./preparing-input";
 export * from "./inference-frame";
+export * from "./split-frames";
